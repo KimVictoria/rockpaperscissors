352f8be6878da3231c07f9b1b653e7803495b501
KimVictoria <kimberlyvmagpantay@icloud.com>
2019-10-22 01:27:16 +0000

    Made a click handler and a slight outline for html

--- js/script.js
+++ js/script.js
@@ -9,3 +9,17 @@
 
 // DOCUMENT READY FUNCTION BELOW
 
+$("#shoot").click(function() {
+    let ____ = $("").val;
+    $("#userChoice").text(____);
+
+});
+
+let r = math.random()
+    if (r<0.333) {
+        let computerchoice= "rock";
+    } else if (r<0.67) {
+        let computerchoice= "paper";
+    } else
+        let computerchoice= "scissors";
+
